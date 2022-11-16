9d58e30ac60adf62271e44cddd8b4c8098c13ea2
Courtney Marasigan <coumardev@gmail.com>
2022-11-16 17:55:59 -0500

    iteration 3

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,18 @@ const User = require('../models/User.model');
 
 const bcryptjs = require('bcryptjs');
 
+const {isLoggedIn, isAnon} = require('../middlewares/auth.middlesware');
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get('/signup', (req, res, next) => {
+router.get('/signup', isAnon, (req, res, next) => {
   res.render('signup.hbs');
 })
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', isAnon, (req, res, next) => {
   console.log(req.body);
   if(!req.body.username || ! req.body.password){
     res.send('Sorry! You forgot to add an email or password.');
@@ -22,7 +24,9 @@ router.post('/signup', (req, res, next) => {
   User.findOne({username:req.body.username})
     .then(foundUser => {
       if(foundUser){
-        res.send('Sorry! User already exists.');
+        // res.send('Sorry! User already exists.');
+        // return;
+        res.render('signup.hbs', {errorMessage: 'Sorry! User already exists.'});
         return;
       }
       return User.create({
@@ -32,7 +36,8 @@ router.post('/signup', (req, res, next) => {
     })
     .then(createdUser => {
       console.log("here's the new user", createdUser);
-      res.send(createdUser);
+      // res.send(createdUser);
+      res.render('login.hbs');
     })
     .catch(err => {
       console.log(err);
@@ -40,11 +45,11 @@ router.post('/signup', (req, res, next) => {
     })
 })
 
-router.get('/login', (req, res, next) => {
+router.get('/login', isAnon, (req, res, next) => {
   res.render('login.hbs');
 });
 
-router.post('/login', (req, res, next) => {
+router.post('/login', isAnon, (req, res, next) => {
   console.log(req.body);
 
     const { username, password } = req.body;
@@ -59,24 +64,24 @@ router.post('/login', (req, res, next) => {
     .then(foundUser => {
 
         if(!foundUser){
-            res.send('Sorry! User does not exist.');
-            // res.render('login.hbs', {errorMessage: 'Sorry! User does not exist.'})
+            // res.send('Sorry! User does not exist.');
+            res.render('login.hbs', {errorMessage: 'Sorry! User does not exist.'})
             return;
         }
 
         const isValidPassword = bcryptjs.compareSync(password, foundUser.password);
 
         if(!isValidPassword){
-            res.send('Sorry! Incorrect password');
-            // res.render('login.hbs', {errorMessage: 'Sorry! Incorrect password.'})
+            // res.send('Sorry! Incorrect password');
+            res.render('login.hbs', {errorMessage: 'Sorry! Incorrect password.'})
             return;
         }
 
         req.session.user = foundUser;
 
         console.log(req.session, "SESSION")
-        res.send('successfully logged in');
-        // res.render('profile.hbs', foundUser)
+        // res.send('successfully logged in');
+        res.render('profile.hbs', foundUser)
     })
     .catch(err => {
         console.log(err);
@@ -84,9 +89,17 @@ router.post('/login', (req, res, next) => {
     });
 });
 
-router.get('/profile', (req, res, next) => {
+router.get('/profile', isLoggedIn, (req, res, next) => {
   console.log(req.session);
   res.render('profile.hbs', req.session.user);
+})
+
+router.get('/main', isAnon, (req, res, next) => {
+  res.render('main.hbs')
+})
+
+router.get('/private', isLoggedIn, (req, res, next) => {
+  res.render('private.hbs')
 })
 
 module.exports = router;

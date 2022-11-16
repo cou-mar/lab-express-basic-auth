9002a2f4c067a3d28dde9d21eb8cf6315ada855e
Courtney Marasigan <coumardev@gmail.com>
2022-11-16 14:52:27 -0500

    iteration 2

--- routes/index.js
+++ routes/index.js
@@ -40,4 +40,53 @@ router.post('/signup', (req, res, next) => {
     })
 })
 
+router.get('/login', (req, res, next) => {
+  res.render('login.hbs');
+});
+
+router.post('/login', (req, res, next) => {
+  console.log(req.body);
+
+    const { username, password } = req.body;
+
+    if(!username || !password){
+        // res.send('Sorry! You forgot an email or password.');
+        res.render('login.hbs', {errorMessage: 'Sorry! You forgot a username or password.'});
+        return;
+    }
+
+    User.findOne({username})
+    .then(foundUser => {
+
+        if(!foundUser){
+            res.send('Sorry! User does not exist.');
+            // res.render('login.hbs', {errorMessage: 'Sorry! User does not exist.'})
+            return;
+        }
+
+        const isValidPassword = bcryptjs.compareSync(password, foundUser.password);
+
+        if(!isValidPassword){
+            res.send('Sorry! Incorrect password');
+            // res.render('login.hbs', {errorMessage: 'Sorry! Incorrect password.'})
+            return;
+        }
+
+        req.session.user = foundUser;
+
+        console.log(req.session, "SESSION")
+        res.send('successfully logged in');
+        // res.render('profile.hbs', foundUser)
+    })
+    .catch(err => {
+        console.log(err);
+        res.send(err);
+    });
+});
+
+router.get('/profile', (req, res, next) => {
+  console.log(req.session);
+  res.render('profile.hbs', req.session.user);
+})
+
 module.exports = router;
